d3c58dbb8b00d05aa6133138b7af594e59f0c790
Rafael Trevisan <rafaelst2000@gmail.com>
2020-12-16 21:20:58 -0300

    cleaning defaults and css reset

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+ul {
+  list-style: none;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+  text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  color: #5a5c69;
+  background-color: #f7f8fb;
+}
+
+.btn {
+  color: #4e73df;
+}
+
+.btn-active {
+  color: #1cc88a;
 }
 </style>
